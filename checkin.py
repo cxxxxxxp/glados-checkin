8161892f5e0de60c35b99ce99dda45af8b5fd9b6
cxxxxxxp <a6235930315@126.com>
2021-06-19 16:38:56 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -23,9 +23,8 @@ def start():
 
     if 'message' in checkin.text:
         mess = checkin.json()['message']
-        #time = state.json()['data']['leftDays']
-        #time = time.split('.')[0]
-        time = "83"
+        time = state.json()['data']['leftDays']
+        time = time.split('.')[0]
         #print(time)
         if sever == 'on':
             requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text='+mess+'&desp=you have '+time+' days left')
